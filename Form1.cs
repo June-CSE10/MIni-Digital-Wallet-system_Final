913bc4476524de7e52ac8f5ae49d035fb06ea1b3
June-CSE10 <methona9656@acledaaib.onmicrosoft.com>
2026-06-17 18:25:19 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace MIni_Digital_Wallet_system_Final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
