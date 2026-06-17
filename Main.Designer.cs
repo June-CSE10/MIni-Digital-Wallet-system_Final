b2dfaeffa39cf31779d8a287aac30335e01660c3
June-CSE10 <methona9656@acledaaib.onmicrosoft.com>
2026-06-17 18:37:24 +0700

    Updated: Login(form1), Register, Main Form.

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace MIni_Digital_Wallet_system_Final
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,20 +23,20 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             SuspendLayout();
             // 
-            // Form1
+            // Main
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "LOGIN";
+            Name = "Main";
+            Text = "MAIN";
             ResumeLayout(false);
         }
 
