799d9a21faa2823a34932d323a79fc70c0d7f6ff
Sovanna67 <sovanna9570@acledaaib.onmicrosoft.com>
2026-06-20 10:54:42 +0700

    New update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MIni_Digital_Wallet_system_Final
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Form1()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
